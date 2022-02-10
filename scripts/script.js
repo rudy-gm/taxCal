dd4227cce5cfce5bbd170e5e151d1c94882e6eee
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-02-10 13:15:43 -0500

    5th commit // modal functionality through script.js (not jquery)

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
+// Get the modal
+let taxModal = document.getElementById("taxResultModal");
+
+// Get the button that opens the modal
+let taxBtn = document.getElementById("calculateButton");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("closeModal")[0];
+
+// When the user clicks the button, open the modal 
+taxBtn.onclick = function() {
+  taxModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  taxModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == taxModal) {
+    taxModal.style.display = "none";
+  }
+}
+
 
